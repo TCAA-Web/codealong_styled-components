c8f08f5174e42c840f243d49ee78ac7bfbe9a94f
TCAA-Web <kfry@techcollege.dk>
2023-08-10 14:56:59 +0200

    initial commit

---
+++ styled/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Wrapper } from './styledComponents/StyledWrapper'
+
+function App() {
+
+  return (
+    <Wrapper>
+      <p>Hej med dig</p>
+    </Wrapper>
+  )
+}
+
+export default App
---
+++ styled/src/styledComponents/StyledWrapper.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+margin: 16px;
+font-size: 2rem;
+width: 50%;
+`
